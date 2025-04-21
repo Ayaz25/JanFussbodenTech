b9d60c1e12bcd1a27dce6f402a660361afb30a56
AyazHasan <81388916+Ayaz25@users.noreply.github.com>
2025-04-21 17:38:30 +0200

    create galarie

---
+++ src/constant/galarie.ts
@@ -0,0 +1,48 @@
+
+const path = "src/assets/galarieBilder"
+const data = [
+    {
+        imagelink: `${path}/bild1.jpg`
+    },
+    {
+        imagelink: `${path}/bild2.jpg`
+    },
+    {
+        imagelink: `${path}/bild3.jpg`
+    },
+    {
+        imagelink: `${path}/bild4.jpg`
+    },
+    {
+        imagelink: `${path}/bild5.jpg`
+    },
+    {
+        imagelink: `${path}/bild6.jpg`
+    },
+    {
+        imagelink: `${path}/bild7.jpg`
+    },
+    {
+        imagelink: `${path}/bild8.jpg`
+    },
+    {
+        imagelink: `${path}/bild9.jpg`
+    },
+    {
+        imagelink: `${path}/bild10.jpg`
+    },
+    {
+        imagelink: `${path}/bild11.jpg`
+    },
+    {
+        imagelink: `${path}/bild12.jpg`
+    },
+    {
+        imagelink: `${path}/bild13.jpg`
+    },
+]
+export function getBilder() {
+
+    return data
+
+}
--- src/frontend-components/Galariepage.tsx
+++ src/frontend-components/Galariepage.tsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
+import { getBilder } from "../constant/galarie";
+import "../css-styles/galariepage.css";
+import PageFooter from "./PageFooter";
+
 const GalariePage: React.FC = () => {
+  const bilder = getBilder();
+  const defaultImage = "src/assets/galarieBilder/bild1.jpg";
+  const [selectedBild, setSelectedBild] = useState(defaultImage);
   return (
     <>
-      <h1> Welcome to Galarie</h1>
+      <div className="galareipage">
+        <div className="galariepage-background"></div>
+        <div className="galarie-content">
+          <div className="glarie-title">
+            <h2>Galarie</h2>
+            <h3>
+              Jeder Boden erzählt eine Geschichte – hier zeigen wir Ihnen echte
+              Beispiele aus unserer Arbeit.
+            </h3>
+            <hr className="line" />
+
+            <div className="selectedImage-container">
+              <img
+                className="selectedImage"
+                src={selectedBild}
+                alt="selected-image"
+              />
+            </div>
+            <div className="image-container">
+              {bilder.map(({ imagelink }, index) => (
+                <div key={index}>
+                  <img
+                    className="image"
+                    onClick={() => setSelectedBild(imagelink)}
+                    src={imagelink}
+                    alt="gallery-image"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <PageFooter />
+      </div>
     </>
   );
 };
